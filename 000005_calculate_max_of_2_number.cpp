4754d9cec2fa461e4a83b2991a542ee303715d83
Akash Kumar <akashkumar888333@gmail.com>
2026-08-19 23:32:13 +0530

    000005_calculate_max_of_2_number.cpp

---
+++ 000005_calculate_max_of_2_number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+    int main()
+{
+    int num1,num2;
+
+    cout<<"enter two numbers : ";
+    cin>>num1>>num2;
+
+    if(num1>num2)
+{
+        cout<<"maximum number is : " <<num1<<endl;
+}
+    else
+{
+        cout<<"maximum number is : " <<num2<<endl;
+}
+
+    return 0;
+}    
