e7304484dad94a782c3c813f059fd81475a36830
Akash Kumar <akashkumar888333@gmail.com>
2026-08-19 23:29:59 +0530

    000002_sum_of_two_number.cpp

---
+++ 000002_sum_of_two_number.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+    int main()
+{
+    int a,b,c;
+    
+    cout<<"enter the first number : ";
+    cin>>a;
+
+    cout<<"enter the second number : ";
+    cin>>b;
+
+    c=a+b;
+    cout<<"sum of two number : " <<c;
+
+    return 0;
+}    
