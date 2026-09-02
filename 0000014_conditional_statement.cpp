c61e19bb180d3a8b21481c0383793368f76d5b64
Akash Kumar <akashkumar888333@gmail.com>
2026-09-02 18:57:44 +0530

    0000014_conditional_statement.cpp

---
+++ 0000014_conditional_statement.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+   int main()
+{
+   int n=40;
+   if(n>=0)
+{  cout<<"n is positive"<<endl;
+
+}
+   else
+{
+   cout<<"n is negative"<<endl;
+
+} 
+   return 0;
+}   
