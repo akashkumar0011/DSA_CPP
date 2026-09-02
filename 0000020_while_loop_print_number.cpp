f385cfedcba20a69fec269583568b4ee3dbfd835
Akash Kumar <akashkumar888333@gmail.com>
2026-09-02 19:01:56 +0530

    0000020_while_loop_print_number.cpp

---
+++ 0000020_while_loop_print_number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+   int main()
+{
+   int n=10;
+   int count=1;
+   while(count<=n)
+{
+   cout<<count<< " ";
+   count++;
+}
+   cout<<endl;
+   return 0;
+}
