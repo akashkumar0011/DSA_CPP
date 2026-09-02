1a2af0426ec0368ee26f20ba61c6d6b60adef557
Akash Kumar <akashkumar888333@gmail.com>
2026-09-02 18:59:58 +0530

    0000017_conditional_statement_grade_point.cpp

---
+++ 0000017_conditional_statement_grade_point.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+   int main()
+{
+   int marks;
+   cout<<"enter the marks : ";
+   cin>>marks;
+   if(marks>=90)
+{
+   cout<<"A"<<endl;
+}
+   else if(marks>=80&&marks<90)
+{
+   cout<<"B"<<endl;
+}
+   else
+{  
+   cout<<"C"<<endl;
+} 
+   return 0;
+}
