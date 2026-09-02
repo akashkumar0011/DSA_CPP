ead740a6c9727fefe82b239f81f82c7da5928d4a
Akash Kumar <akashkumar888333@gmail.com>
2026-09-02 18:58:39 +0530

    0000015_conditional_statement_can_vote.cpp

---
+++ 0000015_conditional_statement_can_vote.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+   
+   int main()
+{
+   int age;
+   cout<<"enter the age : ";
+   cin>>age;
+   if(age>=20)
+{  
+   cout<<"you are vote"<<endl;
+}
+   else
+{
+   cout<<"you can't vote"<<endl;
+}
+   return 0;
+}
